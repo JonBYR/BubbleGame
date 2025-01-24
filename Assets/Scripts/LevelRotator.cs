6f0c15d98558b8a1280a05344b4ec9cd071a4ed5
Student <jon.byrne27@btinternet.com>
2025-01-24 20:16:49 +0000

    LevelRotation

---
+++ Assets/Scripts/LevelRotator.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+public class RotateObjectController : MonoBehaviour
+{
+    public float horizontalSpeed = 10f;
+    public float verticalSpeed = 10f;
+    //Drag the camera object here
+    public Camera cam;
+    
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(h, v, 0);
+    }
+    
+}
