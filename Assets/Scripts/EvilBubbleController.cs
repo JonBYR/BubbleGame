b0e6d75b75e9c1997e87e514cbc924ecf7b4e16f
Student <jon.byrne27@btinternet.com>
2025-01-25 23:11:36 +0000

    Navmesh working

--- Assets/Scripts/EvilBubbleController.cs
+++ Assets/Scripts/EvilBubbleController.cs
@@ -10,6 +10,9 @@ public class EvilBubbleController : MonoBehaviour
     private Rigidbody rb;
     public float forceStrength = 10f;
     private Coroutine move;
+    public float explosionForce = 5f;
+    public float radius = 3f;
+    public float upwards = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +26,28 @@ public class EvilBubbleController : MonoBehaviour
     {
         Debug.Log(agent.pathStatus);
         agent.destination = player.position;
+    }
+    IEnumerator movement(Collision c)
+    {
+        agent.enabled = false; //navmesh and physics do not mesh well so ensure this is turned off
+        rb.isKinematic = false; //kinematic also needs to be turned off
+        Vector3 midpointA = c.collider.gameObject.transform.position;
+        Vector3 midpointB = transform.position;
+        Vector3 average = (midpointA + midpointB) / 2; //get the average of the two midpoints
+        Rigidbody rbBubble = c.collider.gameObject.GetComponent<Rigidbody>();
+        rb.AddExplosionForce(explosionForce, average, radius, upwards, ForceMode.Impulse);
+        rbBubble.AddExplosionForce(explosionForce, average, radius, upwards, ForceMode.Impulse);
+        yield return new WaitForSeconds(1f);
+        agent.enabled = true;
+        rb.isKinematic = true;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.name == "Bubble")
+        {
+            StartCoroutine(movement(collision));
+            
+            
+        }
     }
 }
